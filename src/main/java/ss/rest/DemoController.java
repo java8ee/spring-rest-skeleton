6c1e7fce5fb5a51fc51736499b2ca915f84c34ec
Sergey Meshchankin <java8ee@gmail.com>
2015-04-07 19:53:04 +0300

    Added JSON transformation for GET method

--- src/main/java/ss/rest/DemoController.java
+++ src/main/java/ss/rest/DemoController.java
@@ -1,9 +1,11 @@
 package ss.rest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ss.domain.User;
 
 @Controller
 @RequestMapping(value="/demo")
@@ -11,5 +13,13 @@ public class DemoController {
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody String getData() {
         return "{'data':'data'}";
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public @ResponseBody User getUser(@PathVariable int id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("Username");
+        return user;
     }
 }
