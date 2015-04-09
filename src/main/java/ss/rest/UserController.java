cd75bc4f13aaa0f7777ce3d8e32d05ea8466d2fb
Sergey Meshchankin <java8ee@gmail.com>
2015-04-09 16:55:26 +0300

    Simplify REST controller, move handling to /rest/ URL

--- src/main/java/ss/rest/UserController.java
+++ src/main/java/ss/rest/UserController.java
@@ -1,17 +1,16 @@
 package ss.rest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import ss.domain.User;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping(value="/user")
 public class UserController {
     private final Map<Integer, User> users = new HashMap<Integer, User>();
@@ -24,12 +23,12 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody Collection<User> getData() {
+    public Collection<User> getData() {
         return users.values();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public @ResponseBody User getUser(@PathVariable int id) {
+    public User getUser(@PathVariable int id) {
         return users.get(id);
     }
 }
